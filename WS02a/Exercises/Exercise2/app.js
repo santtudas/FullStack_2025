5f0bee17174328eaad6795e61eca10d8b161afd5
santtudas <146427577+santtudas@users.noreply.github.com>
2025-02-12 11:51:15 +0200

    Ekat vedot

---
+++ WS02a/Exercises/Exercise2/app.js
@@ -0,0 +1,5 @@
+const {add, subtract} = require(/Exercise3/math.js) // Ei toimi
+
+
+console.log("Hello, Node.js!")
+console.log('Addition: ', addition (5, 3));
---
+++ WS02a/Exercises/Exercise3/math.js
@@ -0,0 +1,10 @@
+function addition (number1, number2) {
+  return number1 + " "  + number2;
+}
+
+
+function subtraction (number1, number2) {
+  return number1 + " "  + number2;
+}
+
+module.exports ={addition, subtraction};
